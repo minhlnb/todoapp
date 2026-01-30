a694c9875628e8fa05b855eccffd9bc38e8df44a
minhgaa <133486385+minhgaa@users.noreply.github.com>
2026-01-30 10:07:55 +0700

    Initial Commit

---
+++ TodoApp/TodoAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  TodoAppApp.swift
+//  TodoApp
+//
+//  Created by minhgaa on 30/1/26.
+//
+
+import SwiftUI
+
+@main
+struct TodoAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
